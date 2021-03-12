bd9cb0525af432d071c1457f8dd40fc7c19a8d33
jkelly101 <jenLkelly@gmail.com>
2021-03-12 16:46:58 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "jkelly101",
+    database: "employee_trackerDB"
+  });
+
+  connection.connect( (err) => {
+    if(err) throw err;
+    askQuestions();
+});
+
+// * The command-line application should allow users to:
+
+//   * Add departments, roles, employees
+
+//   * View departments, roles, employees
+
+//   * Update employee roles
+
+// function askQuestions(){
+//     inquirer.prompt([
+//         {
+//             type: "list",
+//             message: "",
+//             name: "choice",
+//             choices: [""]
+//         }
+//     ])
