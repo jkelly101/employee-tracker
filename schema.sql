d5476d6fe00359021ff75900e401186fac72d2b2
jkelly101 <jenLkelly@gmail.com>
2021-03-12 19:25:18 -0500

    added NOT NULL to prevent blanks

--- schema.sql
+++ schema.sql
@@ -1,48 +1,29 @@
--- Design the following database schema containing three tables:
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-CREATE DATABASE employee_tracker_db;
+USE employee_db;
 
-USE employee_tracker_db;
 
--- * **department**:
-
---   * **id** - INT PRIMARY KEY
---   * **name** - VARCHAR(30) to hold department name
-
-CREATE TABLE departments(
+CREATE TABLE department(
 id INTEGER auto_increment NOT NULL,
-name VARCHAR(50),
+name VARCHAR(30) NOT NULL,
+
 PRIMARY KEY(id)
 );
 
--- * **role**:
-
---   * **id** - INT PRIMARY KEY
---   * **title** -  VARCHAR(30) to hold role title
---   * **salary** -  DECIMAL to hold role salary
---   * **department_id** -  INT to hold reference to department role belongs to
-
-CREATE TABLE roles(
+CREATE TABLE role(
 id INTEGER auto_increment NOT NULL,
-title VARCHAR(50),
-salary INTEGER NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(8, 2) NOT NULL,
 department_id INTEGER,
 
 PRIMARY KEY(id)
 );
 
--- * **employee**:
-
---   * **id** - INT PRIMARY KEY
---   * **first_name** - VARCHAR(30) to hold employee first name
---   * **last_name** - VARCHAR(30) to hold employee last name
---   * **role_id** - INT to hold reference to role employee has
---   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-
-CREATE TABLE employees(
+CREATE TABLE employee(
 id INTEGER auto_increment NOT NULL,
-first_name VARCHAR(50),
-last_name VARCHAR(50),
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 role_id INTEGER,
 manager_id INTEGER,
 
